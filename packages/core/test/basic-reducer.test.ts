185867361628fc68e2f2f117dc7ee4df80544f19
jiangweixian <jiangweixian1994@gmail.com>
2020-03-21 23:13:16 +0800

    ➕ (core): renderhook

---
+++ packages/core/test/basic-reducer.test.ts
@@ -0,0 +1,8 @@
+import { useRematchReducer } from '../src/use-rematch-reducer'
+import { renderHook } from '@testing-library/react-hooks'
+
+describe('basic usage of use-rematch-reducer', () => {
+  test('state init should work fine', () => {
+    const hook = 
+  })
+})
