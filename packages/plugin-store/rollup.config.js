9899b06c0f657d14361222e8ddc84c11febb04a8
JiangWeixian <saber_wei@163.com>
2020-03-27 11:40:31 +0800

    🎉 (plugin-store): tada

---
+++ packages/plugin-store/rollup.config.js
@@ -0,0 +1,50 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2'
+import alias from 'rollup-plugin-alias'
+import pkg from './package.json'
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: 'src/index.ts',
+    output: {
+      name: 'plugin-store',
+      file: pkg.browser,
+      format: 'umd',
+    },
+    plugins: [
+      resolve(), // so Rollup can find `ms`
+      commonjs(), // so Rollup can convert `ms` to an ES module
+      typescript({
+        typescript: require('typescript'),
+      }), // so Rollup can convert TypeScript to JavaScript
+      alias({
+        resolve: ['.ts', '.js', '.tsx', '.jsx'],
+        entries: [{ find: '@/', replacement: './src/' }],
+      }),
+    ],
+  },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/index.ts',
+    external: ['ms'],
+    plugins: [
+      typescript(), // so Rollup can convert TypeScript to JavaScript
+      alias({
+        resolve: ['.ts', '.js', '.tsx', '.jsx'],
+        entries: [{ find: '@/', replacement: './src/' }],
+      }),
+    ],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+  },
+]
