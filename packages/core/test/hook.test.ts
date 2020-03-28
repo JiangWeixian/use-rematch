bb1a65cce7e1c9e1f7ea572b474ede92180c6e68
jiangweixian <jiangweixian1994@gmail.com>
2020-03-28 09:59:18 +0800

    ✅ (core): hook

---
+++ packages/core/test/hook.test.ts
@@ -0,0 +1,62 @@
+import { useRematchReducer } from '../src/use-rematch-reducer'
+import { renderHook } from '@testing-library/react-hooks'
+
+describe('hook', () => {
+  test('hook should get state', () => {
+    let name, state, dispatch
+    const useSub = (m, s, d) => {
+      name = m
+      state = s
+      dispatch = d
+    }
+    renderHook(() =>
+      useRematchReducer(
+        {
+          name: 'hook',
+          state: {
+            text: 1,
+          } as { text: number },
+          reducers: {
+            set: v => v,
+          },
+        },
+        { hooks: [useSub] },
+      ),
+    )
+    expect(name).toBe('hook')
+    expect(state.text).toBe(1)
+    expect(dispatch.set).toBeDefined()
+  })
+
+  test('hook should subscribe state', () => {
+    let name, state, dispatch
+    const useSub = (m, s, d) => {
+      name = m
+      state = s
+      dispatch = d
+    }
+    const { result } = renderHook(() =>
+      useRematchReducer(
+        {
+          name: 'hook',
+          state: {
+            text: 1,
+          } as { text: number },
+          reducers: {
+            set: (v, payload) => {
+              return {
+                ...v,
+                ...payload,
+              }
+            },
+          },
+        },
+        { hooks: [useSub] },
+      ),
+    )
+    result.current[1].set({ text: 2 })
+    expect(name).toBe('hook')
+    expect(state.text).toBe(2)
+    expect(dispatch.set).toBeDefined()
+  })
+})
