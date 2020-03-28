58289b74345031ff5c86021044f38f51b5b28875
jiangweixian <jiangweixian1994@gmail.com>
2020-03-28 10:59:51 +0800

    🚧 (usegobal): a beta version

--- packages/use-global/src/use-global.ts
+++ packages/use-global/src/use-global.ts
@@ -1,7 +1,50 @@
-import { useEffect } from 'react'
+import { useEffect, useState, useMemo } from 'react'
+import remove from 'lodash.remove'
 
-const store = {}
+export const store = {}
 
 export const useGlobal = (name: string, state: any, dispatch: any) => {
-  useEffect(() => {})
+  useEffect(() => {
+    store[name].state = state
+    store[name].dispatch = dispatch
+    store[name].callbacks?.forEach((updater: Function) => {
+      updater()
+    })
+  }, [state])
+  useEffect(() => {
+    store[name] = {}
+    return () => {
+      store[name] = undefined
+    }
+  }, [name])
+}
+
+export const useGlobalState = (name: string, callback: (state: any) => any) => {
+  const [clock, update] = useState(0)
+  useEffect(() => {
+    store[name].callbacks = store[name].callbacks.concat(update)
+    return () => {
+      remove(store[name].callbacks, callback => callback == update)
+    }
+  }, [name, clock])
+  const state = store[name].state
+  const partialState = useMemo(() => {
+    return callback(state)
+  }, [callback(state)])
+  return partialState
+}
+
+export const useGlobalDispatch = (name: string, callback: (dispatch: object) => any) => {
+  const [clock, update] = useState(0)
+  const dispatch = store[name].dispatch
+  useEffect(() => {
+    store[name].callbacks = store[name].callbacks.concat(update)
+    return () => {
+      remove(store[name].callbacks, callback => callback == update)
+    }
+  }, [name, clock])
+  const partialDispatch = useMemo(() => {
+    return callback(dispatch)
+  }, [callback(dispatch)])
+  return partialDispatch
 }
