673dc4abcaaa4e594e819fadc1b2eae1fd747f89
jiangWeixian <jiangweixian1994@gmail.com>
2022-04-30 21:59:56 +0800

    :arrow_up:  package use vitest config

--- packages/use/test/basic.test.ts
+++ packages/use/test/basic.test.ts
@@ -4,9 +4,7 @@ import { renderHook, act } from '@testing-library/react-hooks'
 
 describe('set', () => {
   it('dispatch set should overwrite original state', () => {
-    const { result } = renderHook(() =>
-      useStore(),
-    )
+    const { result } = renderHook(() => useStore())
     act(() => {
       result.current.dispatch.set({ count: 1 })
     })
--- packages/use/vite.config.ts
+++ packages/use/vite.config.ts
@@ -2,12 +2,10 @@
 /// <reference types="vite/client" />
 
 import path from 'path'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
